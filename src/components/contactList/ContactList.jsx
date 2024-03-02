d09462fb0f1748d4acdd1f1a91646a339bbd5c3e
Dan <danretegan@yahoo.com>
2024-03-02 09:19:24 +0200

    add loader

--- src/components/contactList/ContactList.jsx
+++ src/components/contactList/ContactList.jsx
@@ -1,38 +1,54 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './ContactList.module.css';
 import ContactItem from '../contactItem/ContactItem';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact, fetchContacts } from '../../redux/operations';
-import { selectFilteredContacts } from '../../redux/selectors';
+import { selectFilteredContacts, selectIsLoading } from '../../redux/selectors';
 import PropTypes from 'prop-types';
+import { Loader } from '../loader/loader';
 
 const ContactList = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(selectFilteredContacts);
+  const isLoading = useSelector(selectIsLoading);
 
   const handleDeleteContact = contactId => {
     dispatch(deleteContact(contactId));
   };
 
+  const [showLoader, setShowLoader] = useState(true);
+
   useEffect(() => {
     dispatch(fetchContacts());
+
+    // Afișează loader-ul pentru 1 secundă chiar dacă datele se încarcă mai repede:
+    const loaderTimeout = setTimeout(() => {
+      setShowLoader(false);
+    }, 1000);
+
+    // Anulează timeout-ul la demontare:
+    return () => clearTimeout(loaderTimeout);
   }, [dispatch]);
 
   return (
     <div className={styles.container}>
-      <ul className={styles.label}>
-        {contacts && contacts.length > 0 ? (
-          contacts.map(contact => (
-            <ContactItem
-              key={contact.id}
-              contact={contact}
-              onDeleteContact={handleDeleteContact}
-            />
-          ))
-        ) : (
-          <p>No contacts available.</p>
-        )}
-      </ul>
+      {showLoader && <Loader />}{' '}
+      {/* Afișează Loader-ul doar dacă showLoader este true */}
+      {!showLoader && (
+        <ul className={styles.label}>
+          {contacts && contacts.length > 0 ? (
+            contacts.map(contact => (
+              <ContactItem
+                key={contact.id}
+                contact={contact}
+                onDeleteContact={handleDeleteContact}
+              />
+            ))
+          ) : (
+            <p>No contacts available.</p>
+          )}
+        </ul>
+      )}
     </div>
   );
 };
