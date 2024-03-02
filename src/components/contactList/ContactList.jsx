0928fb661d567bd91a0da5a10de66d34b2db6b55
Dan <danretegan@yahoo.com>
2024-03-02 10:14:03 +0200

    Update ContactList.jsx

--- src/components/contactList/ContactList.jsx
+++ src/components/contactList/ContactList.jsx
@@ -1,40 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import styles from './ContactList.module.css';
 import ContactItem from '../contactItem/ContactItem';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact, fetchContacts } from '../../redux/operations';
-import { selectFilteredContacts, selectIsLoading } from '../../redux/selectors';
+import {
+  selectError,
+  selectFilteredContacts,
+  selectIsLoading,
+} from '../../redux/selectors';
 import PropTypes from 'prop-types';
 import { Loader } from '../loader/loader';
 
 const ContactList = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(selectFilteredContacts);
+  const error = useSelector(selectError);
   const isLoading = useSelector(selectIsLoading);
 
   const handleDeleteContact = contactId => {
     dispatch(deleteContact(contactId));
   };
 
-  const [showLoader, setShowLoader] = useState(true);
-
   useEffect(() => {
     dispatch(fetchContacts());
-
-    // Afișează loader-ul pentru 1 secundă chiar dacă datele se încarcă mai repede:
-    const loaderTimeout = setTimeout(() => {
-      setShowLoader(false);
-    }, 1000);
-
-    // Anulează timeout-ul la demontare:
-    return () => clearTimeout(loaderTimeout);
   }, [dispatch]);
 
   return (
     <div className={styles.container}>
-      {showLoader && <Loader />}{' '}
-      {/* Afișează Loader-ul doar dacă showLoader este true */}
-      {!showLoader && (
+      {isLoading && <Loader />}{' '}
+      {/* Afișează Loader-ul doar dacă isLoading este true */}
+      {!isLoading && !error && (
         <ul className={styles.label}>
           {contacts && contacts.length > 0 ? (
             contacts.map(contact => (
@@ -48,6 +43,11 @@ const ContactList = () => {
             <p>No contacts available.</p>
           )}
         </ul>
+      )}
+      {error && (
+        <p style={{ color: 'red' }}>
+          An error occurred while fetching contacts.
+        </p>
       )}
     </div>
   );
