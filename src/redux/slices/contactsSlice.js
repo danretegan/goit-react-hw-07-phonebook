34f99719d31fbbcb8c1630467a8510f3fb5892b5
Dan <danretegan@yahoo.com>
2024-03-03 09:43:36 +0200

    Update contactsSlice.js

--- src/redux/slices/contactsSlice.js
+++ src/redux/slices/contactsSlice.js
@@ -13,41 +13,47 @@ const contactsSlice = createSlice({
   reducers: {},
   extraReducers: builder => {
     builder
+
+      // fetchContacts:
       .addCase(fetchContacts.pending, state => {
         state.isLoading = true;
+      })
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.items = action.payload;
+        state.error = null;
       })
       .addCase(fetchContacts.rejected, (state, action) => {
         state.isLoading = false;
         state.error = action.error.message;
       })
-      .addCase(fetchContacts.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.error = null;
-        state.items = action.payload;
-      })
+
+      // addContact:
       .addCase(addContact.pending, state => {
         state.isLoading = true;
+      })
+      .addCase(addContact.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.items.push(action.payload);
+        state.error = null;
       })
       .addCase(addContact.rejected, (state, action) => {
         state.isLoading = false;
         state.error = action.error.message;
       })
-      .addCase(addContact.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.error = null;
-        state.items.push(action.payload);
-      })
+
+      // deleteContact:
       .addCase(deleteContact.pending, state => {
         state.isLoading = true;
+      })
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.items = state.items.filter(item => item.id !== action.payload.id);
+        state.error = null;
       })
       .addCase(deleteContact.rejected, (state, action) => {
         state.isLoading = false;
         state.error = action.error.message;
-      })
-      .addCase(deleteContact.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.error = null;
-        state.items = state.items.filter(item => item.id !== action.payload.id);
       });
   },
 });
