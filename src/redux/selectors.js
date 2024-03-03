259d864ac9cb3530bb72bfe4b1d773aae47d5bd0
Dan <danretegan@yahoo.com>
2024-03-03 10:40:17 +0200

    add comments

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -6,14 +6,22 @@ export const selectContacts = state => state.contacts.items;
 // Selector pentru a obține filtrul:
 export const selectFilter = state => state.filter;
 
+// Selector pentru a obține eroarea asociată contactelor:
 export const selectError = state => state.contacts.error;
 
+// Selector pentru a verifica dacă există o încărcare în curs pentru contacte:
 export const selectIsLoading = state => state.contacts.isLoading;
 
-// Selector pentru a filtra contactele în funcție de șirul de căutare:
+// ==== Selector pentru a filtra contactele în funcție de șirul de căutare: ====
+
+// Se definește si se exporta un selector numit 'selectFilteredContacts' utilizând createSelector:
 export const selectFilteredContacts = createSelector(
-  [selectContacts, selectFilter],
+  [selectContacts, selectFilter], // Se specifică inputurile pentru selector (lista de contacte și filtrul)
+
+  // Funcție de filtrare a contactelor:
   (contacts, filter) => {
+
+    // Se returneaza o listă de contacte filtrată pe baza șirului de căutare:
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
